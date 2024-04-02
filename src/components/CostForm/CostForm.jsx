de60e99f5f87494ce653a76e2d3165683ffbefde
Igor Sechinov <112351537+igasin@users.noreply.github.com>
2024-04-02 16:53:43 +0300

    feat(costs): modify styles

--- src/components/CostForm/CostForm.jsx
+++ src/components/CostForm/CostForm.jsx
@@ -1,20 +1,46 @@
+import { useState } from 'react'
 import styles from './CostForm.module.scss'
 
 export const CostForm = () => {
+	const [name, setName] = useState('')
+	const [amount, setAmount] = useState('')
+	const [date, setDate] = useState('')
+
+	const nameChangeHandler = event => {
+		setName(event.target.value)
+	}
+	const amountChangeHandler = event => {
+		setAmount(event.target.value)
+	}
+
+	const dateChangeHandler = event => {
+		setDate(event.target.value)
+	}
+
 	return (
 		<form>
 			<div className={styles.newCostControls}>
 				<div className={styles.newCostControl}>
 					<label>Name</label>
-					<input type='text' />
+					<input type='text' onChange={nameChangeHandler} />
 				</div>
 				<div className={styles.newCostControl}>
 					<label>Amount</label>
-					<input type='number' min='0.01' step='0.01' />
+					<input
+						type='number'
+						min='0.01'
+						step='0.01'
+						onChange={amountChangeHandler}
+					/>
 				</div>
 				<div className={styles.newCostControl}>
 					<label>Date</label>
-					<input type='date' min='2019-01-01' step='2022-12-31' />
+					<input
+						type='date'
+						min='2019-01-01'
+						step='2022-12-31'
+						onChange={dateChangeHandler}
+					/>
 				</div>
 				<div className={styles.newCostActions}>
 					<button type='submit'>Add Cost</button>
